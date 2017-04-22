84052e94387d0f0cbd3301c84610eed9bfb69ba3
bo brinkman <dr.bo.brinkman@gmail.com>
2017-04-22 10:31:20 -0400

    Keep a variable in case I need it later

--- js/screens/play.js
+++ js/screens/play.js
@@ -9,8 +9,9 @@ game.PlayScreen = me.ScreenObject.extend({
 	me.game.world.addChild(new me.ColorLayer("background", "#00003f"), 0);
 	
 	var ssize = me.game.viewport.width / 3;
-	me.game.world.addChild(new game.DanceBox(me.game.viewport.width/2 - ssize/2, 
-						 me.game.viewport.height/2 - ssize/2, ssize, ssize));
+	this.DB = new game.DanceBox(me.game.viewport.width/2 - ssize/2, 
+	    me.game.viewport.height/2 - ssize/2, ssize, ssize);
+	me.game.world.addChild(this.DB);
 
 	// Add our HUD to the game world, add it last so that this is on top of the rest.
         // Can also be forced by specifying a "Infinity" z value to the addChild function.
@@ -22,6 +23,7 @@ game.PlayScreen = me.ScreenObject.extend({
      *  action to perform when leaving this screen (state change)
      */
     onDestroyEvent: function() {
+	me.game.world.removeChild(this.DB);
 	// remove the HUD from the game world
         me.game.world.removeChild(this.HUD);
     }
