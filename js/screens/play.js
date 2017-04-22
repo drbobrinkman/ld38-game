eba98afa7f5c8137948c9d801074130be01eec4d
bo brinkman <dr.bo.brinkman@gmail.com>
2017-04-21 22:28:54 -0400

    Add the first attempt at waltz music

--- js/screens/play.js
+++ js/screens/play.js
@@ -6,7 +6,10 @@ game.PlayScreen = me.ScreenObject.extend({
         // reset the score
         game.data.score = 0;
 
-        // Add our HUD to the game world, add it last so that this is on top of the rest.
+	//Start the soundtrack. TODO: change to the intro music
+        me.audio.playTrack("MainBoxStep");
+
+	// Add our HUD to the game world, add it last so that this is on top of the rest.
         // Can also be forced by specifying a "Infinity" z value to the addChild function.
         this.HUD = new game.HUD.Container();
         me.game.world.addChild(this.HUD);
@@ -16,7 +19,10 @@ game.PlayScreen = me.ScreenObject.extend({
      *  action to perform when leaving this screen (state change)
      */
     onDestroyEvent: function() {
-        // remove the HUD from the game world
+	// stop the music
+	me.audio.stopTrack();
+	
+	// remove the HUD from the game world
         me.game.world.removeChild(this.HUD);
     }
 });
