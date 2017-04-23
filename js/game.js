74a691180c5615a6f358428631226a9164f924e6
bo brinkman <dr.bo.brinkman@gmail.com>
2017-04-23 09:29:56 -0400

    Upgrade resolution

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ var game = {
     // Run on page load.
     "onload" : function () {
         // Initialize the video.
-        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto", scaleMethod : "fill-max"})) {
+        if (!me.video.init(1920, 1080, {wrapper : "screen", scale : "auto", scaleMethod : "fit"})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
